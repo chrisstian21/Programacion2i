72fadba322d9063b26a4713bbcb0c71da5cb03bd
Christian <christiandanilo104@gmail.com>
2025-05-07 09:14:29 -0500

    Poyecto5

---
+++ Proyecto 1/Proyecto1.py
@@ -0,0 +1,7 @@
+#Solicita al usuario su nombre y su año de nacimiento
+nombre = input("Ingrese su nombre: ")
+año_de_nacimiento = int(input("Ingrese su fecha de nacimiento: "))
+print("\n")
+#Calcular la edad aproximada
+año_actual = 2025 
+edad = año_actual - año_de_nacimiento 
